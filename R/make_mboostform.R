8552291a4906a670b0df6ebd4804a23b25ea4258
mayrandy <andreas.mayr@fau.de>
2016-11-18 22:01:23 +0100

    build mboost formulas

---
+++ R/make_mboostform.R
@@ -0,0 +1,22 @@
+#
+#
+#
+# 
+
+
+make_mboostform <- function(formula)
+{
+  form <- as.formula(formula)
+  labs <- attr(terms.formula(form), "term.labels")
+  
+  ns <- sapply(labs, function(x) grepl(substr(x, 1, 2), pattern = "s\\(") ) 
+  if(! any(ns)) return(form)
+  else{
+    labs[ns] <-   gsub(x = labs[ns], "s\\(", "bbs\\(")
+    labs[!ns] <- paste("bols(", labs[!ns], ", intercept = FALSE)", sep = "")
+  }
+  rform <- paste(paste(all.vars(form[[2]]), " ~ ", sep = ""), 
+                 paste(labs, collapse = " + "))
+  return(as.formula(rform))
+}
+  
